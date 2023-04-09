4b448f452ea63a66ff135af126501e08ef1e7135
codeboyzhou <imzhouchen@gmail.com>
2023-04-10 00:13:37 +0800

    feature(core): Support using apache httpclient5 library to send api request

---
+++ src/main/java/io/github/hello/openai/http/ApacheHttpClient5.java
@@ -0,0 +1,46 @@
+package io.github.hello.openai.http;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * @author codeboyzhou <imzhouchen@gmail.com>
+ * @since 0.1.0
+ */
+public class ApacheHttpClient5 implements HttpClientSupport {
+
+    private final HttpClientBuilder httpClientBuilder;
+
+    private Map<String, String> headers;
+
+    public ApacheHttpClient5() {
+        httpClientBuilder = HttpClientBuilder.create();
+    }
+
+    @Override
+    public void addHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    @Override
+    public void proxy(String host, int port) {
+        HttpHost proxy = new HttpHost(host, port);
+        httpClientBuilder.setProxy(proxy);
+    }
+
+    @Override
+    public String get(String url) throws IOException {
+        HttpGet httpGet = new HttpGet(url);
+        headers.forEach(httpGet::addHeader);
+        try (CloseableHttpClient httpClient = httpClientBuilder.build()) {
+            return httpClient.execute(httpGet, response -> EntityUtils.toString(response.getEntity()));
+        }
+    }
+
+}
